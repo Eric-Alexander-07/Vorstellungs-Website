70b5327bbf0f358795011efc03e2c4e03ec0e8ae
Eric-Alexander-07 <150048451+Eric-Alexander-07@users.noreply.github.com>
2024-01-30 19:35:45 +0100

    Add files via upload

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -26,56 +26,56 @@ document.querySelector('.sidebar').innerHTML = `
 <a href="#">
   <img onclick="
     openSidebar();
-  " class="bx bx-menu" id="btn" src="./images/sidebar/menu.png">
+  " class="bx bx-menu sidebar-icons" id="btn" src="./images/sidebar/menu.png">
 </a>
 <a href="./main.html" class="logo_hidden">Eric</a>
 </div>
 <ul class="nav-list">
 <li>
   <a href="./weitere-dokumente.html">
-    <img src="./images/sidebar/document.png">
+    <img class="sidebar-icons" src="./images/sidebar/document.png">
     <span class="links_name">Weitere Dokumente</span>
   </a>
   <span class="tooltip">Weitere Dokumente</span>
 </li>
 <li>
   <a href="./sport.html">
-    <img src="./images/sidebar/fitness.png">
+    <img class="sidebar-icons" src="./images/sidebar/fitness.png">
     <span class="links_name">Sport</span>
   </a>
   <span class="tooltip">Sport</span>
 </li>
 <li>
   <a href="./musik.html">
-    <img src="./images/sidebar/music.png">
+    <img class="sidebar-icons" src="./images/sidebar/music.png">
     <span class="links_name">Musik</span>
   </a>
   <span class="tooltip">Musik</span>
 </li>
 <li>
   <a href="./trading.html">
-    <img src="./images/sidebar/stocks.png">
+    <img class="sidebar-icons" src="./images/sidebar/stocks.png">
     <span class="links_name">Trading</span>
   </a>
   <span class="tooltip">Trading</span>
 </li>
 <li>
   <a href="./programmieren.html">
-    <img src="./images/sidebar/terminal.png"></img>
+    <img class="sidebar-icons" src="./images/sidebar/terminal.png"></img>
     <span class="links_name">Programmieren</span>
   </a>
   <span class="tooltip">Programmieren</span>
 </li>
 <li>
   <a href="./ehemalige-praktika.html">
-    <img src="./images/sidebar/factory.png">
+    <img class="sidebar-icons" src="./images/sidebar/factory.png">
     <span class="links_name">Ehemalige Praktika</span>
   </a>
   <span class="tooltip">Ehemalige Praktika</span>
 </li>
 <li>
   <a href="./kontaktdaten.html">
-    <img src="./images/sidebar/qr-code.png">
+    <img class="sidebar-icons" src="./images/sidebar/qr-code.png">
     <span class="links_name">Kontaktdaten</span>
   </a>
   <span class="tooltip">Kontaktdaten</span>
