efcecc78677296d95469584297b4c7598c157a63
Eric-Alexander-07 <150048451+Eric-Alexander-07@users.noreply.github.com>
2024-02-03 15:45:27 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,24 @@ let searchBtn = document.querySelector(".bx-search");
 closeBtn.addEventListener("click", ()=>{
   sidebar.classList.toggle("open");
 });
+
+function swipe() {
+  let largeImage = this.document.getElementById('largeImage');
+  let url = largeImage.getAttribute('src');
+  window.open(url);
+}
+function swipe2() {
+  let largeImage = this.document.getElementById('largeImage2');
+  let url = largeImage.getAttribute('src');
+  window.open(url);
+}
+function swipe3() {
+  let largeImage = this.document.getElementById('largeImage3');
+  let url = largeImage.getAttribute('src');
+  window.open(url);
+}
+function swipe4() {
+  let largeImage = this.document.getElementById('largeImage4');
+  let url = largeImage.getAttribute('src');
+  window.open(url);
+}
